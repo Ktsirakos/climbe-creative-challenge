da70ebdae57ff717ced89e4dc57b580a25033647
Konstantinos Tsirakos <tkonstantinos1996@hotmail.com>
2021-09-08 19:03:25 +0100

    Finished connect four with already existing test successful

--- candidate-challenges/connect-four/src/helpers.ts
+++ candidate-challenges/connect-four/src/helpers.ts
@@ -5,8 +5,25 @@ export function getColumnFromCharacter(value: string): number {
   return availableCharacters.indexOf(value);
 }
 
-export function convertDiagonalsIntoRows(
-  board: ConnectFourBoardGame
-): string[] {
-  return [""];
+export function convertDiagonalsIntoRows(board: string[][]): string[] {
+  const diagonals = ["", "", "", "", "", "", "", "", "", "", "", "", ""];
+  for (let value = -5; value < 5; value++) {
+    for (let i = 0; i < 7; i++) {
+      for (let j = 0; j < 6; j++) {
+        if (i - j === value) {
+          diagonals[value + 5] += board[i][j];
+        }
+      }
+    }
+  }
+
+  return diagonals.filter((elem) => elem.length >= 4);
+}
+
+export function mirrorArrayFromRows(boardRepresentation: string[][]) {
+  const newData = [];
+  for (var i = 0; i < boardRepresentation.length; i++) {
+    newData.push(boardRepresentation[boardRepresentation.length - (i + 1)]);
+  }
+  return newData;
 }
--- candidate-challenges/connect-four/src/solution.ts
+++ candidate-challenges/connect-four/src/solution.ts
@@ -1,5 +1,9 @@
 import { ConnectFourBoardGame } from "./datamodel/datamodel";
-import { getColumnFromCharacter } from "./helpers";
+import {
+  convertDiagonalsIntoRows,
+  getColumnFromCharacter,
+  mirrorArrayFromRows,
+} from "./helpers";
 
 enum Winner {
   Yellow = "Yellow",
@@ -14,54 +18,39 @@ export enum Players {
 
 export function whoIsWinner(piecesPositionList: string[]): Winner {
   const game = new ConnectFourBoardGame();
-  
+
   for (const move of piecesPositionList) {
     const [index, player] = move.split("_");
-    console.log(index, player);
     const winner = game.addMoveToPlayer(
       player === "Yellow" ? Players.Yellow : Players.Red,
       getColumnFromCharacter(index)
     );
 
     if (winner) {
-      console.log("We have a winner", winner);
       return winner === "Y" ? Winner.Yellow : Winner.Red;
     }
 
-    console.log(game.getBoardRepresentation());
-  }
+    //Check diagonals for winner
+    const diagonals = [
+      ...convertDiagonalsIntoRows(game.getBoardRepresentation()),
+      ...convertDiagonalsIntoRows(
+        mirrorArrayFromRows(game.getBoardRepresentation())
+      ),
+    ];
 
+    if (
+      diagonals.includes(
+        `${Players.Yellow}${Players.Yellow}${Players.Yellow}${Players.Yellow}`
+      )
+    ) {
+      return Winner.Yellow;
+    } else if (
+      diagonals.includes(
+        `${Players.Red}${Players.Red}${Players.Red}${Players.Red}`
+      )
+    ) {
+      return Winner.Red;
+    }
+  }
   return Winner.Draw;
 }
-
-whoIsWinner([
-  "C_Yellow",
-  "E_Red",
-  "G_Yellow",
-  "B_Red",
-  "D_Yellow",
-  "B_Red",
-  "B_Yellow",
-  "G_Red",
-  "C_Yellow",
-  "C_Red",
-  "D_Yellow",
-  "F_Red",
-  "E_Yellow",
-  "A_Red",
-  "A_Yellow",
-  "G_Red",
-  "A_Yellow",
-  "F_Red",
-  "F_Yellow",
-  "D_Red",
-  "B_Yellow",
-  "E_Red",
-  "D_Yellow",
-  "A_Red",
-  "G_Yellow",
-  "D_Red",
-  "D_Yellow",
-  "C_Red",
-]);
-//Create class for the Stack
